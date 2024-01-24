31810c604bc1d14ddc1616c50be45371c5da7815
Izaak Dale <izaakdaledev@gmail.com>
2024-01-23 21:27:48 -0800

    added custom deploy action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,8 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1. get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2 upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${region}`);
+
   core.notice('hello from custom js action');
 }
 
